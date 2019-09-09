3d2bc9e0197924e4d85013e5c6c96f7de6bedb75
JetskevdWouden <zegmaarjet@gmail.com>
2019-09-09 14:01:21 +0200

    homepage

---
+++ src/components/Homepage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+// import { connect } from 'react-redux';
+
+class Homepage extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Squash It Game</h1>
+            </div>
+        )
+    }
+}
+
+// const mapStateToProps = (state) => {
+//     return {
+
+//     }
+// }
+
+// export default connect(mapStateToProps)(Homepage);
+
+export default Homepage;
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { withRouter } from 'react-router';
 import { connect } from 'react-redux';
+import Homepage from './Homepage';
 
 function Routes(props) {
     return (
         <div>
-            {/* <Route path='/' exact comnponent={} /> */}
+            <Route path='/home' exact component={Homepage} />
         </div>
     )
 }
